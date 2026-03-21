8e62e04c469099e7793aa8b283c2e7bd237537f0
ayazoadel <ayazoadel@gmail.com>
2026-03-21 18:33:45 -0300

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -118,7 +118,7 @@ class AuditLogResponse(BaseModel):
     action:          str
     old_value:       Optional[str]
     new_value:       Optional[str]
-    created_at:      str
+    created_at:      datetime
 
 
 # ── GENERADOR ─────────────────────────────────────────────────
